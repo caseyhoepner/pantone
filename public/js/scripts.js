d1579dccd9395729eac893d759732802ef9b1d33
Casey Hoepner <caseyhoepner@gmail.com>
2018-11-28 18:31:35 -0700

    Hook up save palette button to generate a post object

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,22 +1,38 @@
+let randomColors;
 
 const generateColors = () => {
-  console.log('generating colors')
-  $('.color1').css('background-color', getRandomColor())
-  $('.color2').css('background-color', getRandomColor())
-  $('.color3').css('background-color', getRandomColor())
-  $('.color4').css('background-color', getRandomColor())
-  $('.color5').css('background-color', getRandomColor())
+  randomColors = [
+    getRandomColor(),
+    getRandomColor(),
+    getRandomColor(),
+    getRandomColor(),
+    getRandomColor(),
+  ]
+
+  $('.color1').css('background-color', randomColors[0])
+  $('.color2').css('background-color', randomColors[1])
+  $('.color3').css('background-color', randomColors[2])
+  $('.color4').css('background-color', randomColors[3])
+  $('.color5').css('background-color', randomColors[4])
 };
 
 $(window).on("load", generateColors);
 $('.new-palette-btn').on('click', generateColors);
 
 const getRandomColor = () => {
-  return (
-    "#" +
-    Math.random()
-      .toString(16)
-      .slice(2, 8)
-  );
+  return "#" +Math.random().toString(16).slice(2, 8)
 };
 
+const savePalette = () => {
+  const newPalette = {
+    name: $('.palette-name').val(),
+    color1: randomColors[0],
+    color2: randomColors[1],
+    color3: randomColors[2],
+    color4: randomColors[3],
+    color5: randomColors[4]
+  }
+  console.log(newPalette);
+}
+
+$('.save-palette-btn').on('click', savePalette);
